bfd75af11e2b307dfc4114ed79c04500985c7ba0
randyglasgow <glasgowrandy@gmail.com>
2021-02-06 17:26:40 -0500

    Updates Base app to more accepted format.

--- App.js
+++ App.js
@@ -1,30 +1,20 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-import { withAuthenticator } from 'aws-amplify-react-native'
-
-import Amplify from '@aws-amplify/core'
-import config from './src/aws-exports'
-Amplify.configure(config)
-
-class App extends React.Component {
-  render() {
-    return (
-        <View style={styles.container}>
-          <Text>Open up App.js to start working on your app!</Text>
-          <Text>Yo!</Text>
-        </View>
-    );
-  }
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Yo!</Text>
+    </View>
+  );
 }
-
-export default withAuthenticator(App, { includeGreetings: true })
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
